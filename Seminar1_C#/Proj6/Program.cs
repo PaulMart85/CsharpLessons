f44cc61dd9b9e8d240f02cac116b19cefb456749
paul <outimer@mail.ru>
2022-02-26 12:43:51 +0600

    even numbers from 1 to N task added

---
+++ Seminar1_C#/Proj6/Program.cs
@@ -0,0 +1,28 @@
+﻿// Task: all even numbers from 1 to N in question
+Console.Clear();
+
+Console.Write("Input the number: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int even = 1;
+
+Console.Write(N + " -> ");
+
+if (N > 0) {
+    while (even <= N) {
+        if (even%2 == 0) {
+            Console.Write(even + ", ");
+        }
+        even++;
+    }
+}
+else {
+     while (even >= N) {
+        if (even%2 == 0) {
+            Console.Write(even + ", ");
+        }
+        even--;
+    }
+}
+
+Console.WriteLine();
