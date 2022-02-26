b0fcd7d834f874b67c26754211cfbb093ac90876
paul <outimer@mail.ru>
2022-02-26 11:59:34 +0600

    greater number between two task added

---
+++ Seminar1_C#/Proj3/Program.cs
@@ -0,0 +1,16 @@
+﻿// Task: which number between two is greater
+Console.Clear();
+
+Console.Write("Input the first number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input the second number: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int max = b;
+
+if (a > b) {
+    max = a;
+}
+
+Console.WriteLine("a = " + a + "; b = " + b + " -> max = " + max);
