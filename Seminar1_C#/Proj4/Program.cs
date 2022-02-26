84be2562e0c468a900985a5765063cea30d3f22a
paul <outimer@mail.ru>
2022-02-26 12:13:02 +0600

    greater number among three task added

---
+++ Seminar1_C#/Proj4/Program.cs
@@ -0,0 +1,22 @@
+﻿// Task: which number among three is greater
+Console.Clear();
+
+Console.Write("Input the first number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input the second number: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input the third number: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+
+if (max < b) {
+    max = b;
+}
+if (max < c) {
+    max = c;
+}
+
+Console.WriteLine(a + ", " + b + ", " + c + " -> " + max);
