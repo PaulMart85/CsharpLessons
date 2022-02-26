f066281bb1a8893cfc59213dd26f4c5441c395fe
paul <outimer@mail.ru>
2022-02-26 12:25:26 +0600

    if even number task added

---
+++ Seminar1_C#/Proj5/Program.cs
@@ -0,0 +1,12 @@
+﻿// Task: is it an even number?
+Console.Clear();
+
+Console.Write("Input the number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a%2 == 0) {
+    Console.WriteLine(a + " -> Yes");
+}
+else {
+    Console.WriteLine(a + " -> No");
+}
