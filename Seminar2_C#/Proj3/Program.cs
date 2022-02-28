3738957825f176bc02ef3f3ecb27b9247cdedcda
paul <outimer@mail.ru>
2022-02-28 18:24:24 +0600

    if number of weekday is weekend task added

---
+++ Seminar2_C#/Proj3/Program.cs
@@ -0,0 +1,27 @@
+﻿// Task: if the number of the week is a weekend
+Console.Clear();
+
+string IsItWeekeend(int num) {
+
+    if (num < 1 || num > 7) return "No such a weekday's number!";
+
+    if (num == 6 || num == 7) {
+        return "Yes, it is a weekend!";
+    }
+    else {
+        return "No, it is not a weekend!";
+    }
+}
+
+Console.Write("Input the number of the weekday (or input R for a random choice): ");
+string ver = Console.ReadLine().ToLower();
+int numberOfWeekday;
+
+if (ver == "r") {
+    numberOfWeekday = new Random().Next(1,8);
+}
+else {
+    numberOfWeekday = Convert.ToInt32(ver);
+}
+
+Console.WriteLine(numberOfWeekday + " -> " + IsItWeekeend(numberOfWeekday));
