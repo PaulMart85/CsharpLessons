c9b5672f5338b222d42768b1fef46549aaf1b219
paul <outimer@mail.ru>
2022-02-28 18:06:41 +0600

    third digit of number task added

---
+++ Seminar2_C#/Proj2/Program.cs
@@ -0,0 +1,32 @@
+﻿//Task: third digit of number
+Console.Clear();
+
+int ThirdDigitOfNumber(int num){
+    int ver = num;
+    int cnt = 0;
+
+    do{
+        ver = ver/10;
+        cnt++;
+    }
+    while(ver >= 1 || ver <= -1);
+
+    if(cnt < 3) {
+        Console.Write("There is no third digit: ");
+        return -1;
+    }
+    else return Math.Abs(num/Convert.ToInt32(Math.Pow(10,cnt-3)) % 10);
+}
+
+Console.Write("Input any number (or input R for random): ");
+string number = Console.ReadLine().ToLower();
+int givenNumber;
+
+if (number == "r") {
+    givenNumber = new Random().Next(-100000,100000);
+}
+else {
+    givenNumber = Convert.ToInt32(number);
+}
+
+Console.WriteLine(givenNumber + " -> " + ThirdDigitOfNumber(givenNumber));
