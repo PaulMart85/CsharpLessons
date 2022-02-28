d26c3e599cd76e3e19baded03a689154c14f241c
paul <outimer@mail.ru>
2022-02-28 16:01:54 +0600

    second digit of number task added

---
+++ Seminar2_C#/Proj1/Program.cs
@@ -0,0 +1,23 @@
+﻿// Task: second digit of three-digit number
+Console.Clear();
+
+int SecDigitOfNumber(int num){
+    if (num/100 < 1 || num/100 > 9) {
+        Console.Write("It is not a three-digit number: ");
+        return -1;
+    }
+    else return (num/10)%10;
+}
+
+Console.Write("Input any three-digit number (or input R for random): ");
+string number = Console.ReadLine().ToLower();
+int givenNumber;
+
+if (number == "r") {
+    givenNumber = new Random().Next(100,1000);
+}
+else {
+    givenNumber = Convert.ToInt32(number);
+}
+
+Console.WriteLine(givenNumber + " -> " + SecDigitOfNumber(givenNumber));
