b8fdac85ff886835022ae3405f7e13ba26d0ec5e
paul <outimer@mail.ru>
2022-03-03 17:28:05 +0600

    if 5-digit number is palindrome task added

---
+++ Seminar3_C#/Proj4/Program.cs
@@ -0,0 +1,38 @@
+﻿// Task: if the five-digit number is a palindrome
+Console.Clear();
+
+string ifNumberIsPalindrome(int numb)
+{
+    int cnt = 0, ver = numb;
+    do
+    {
+        ver = ver/10;
+        cnt++;
+    } while (ver >=1);
+
+    int[] array = new int[cnt];
+    int index = 0;
+
+    while (index < cnt) 
+    {
+        array[index++] = numb%10;
+        numb = numb/10;
+    }
+    
+    //int[cnt] array = {numb/10000, numb/1000%10, numb/100%10, numb/10%10, numb%10};
+    index = 0;
+    while (index < cnt/2) 
+    {
+        if (array[index] != array[cnt-1 - index++]) return "No"; 
+    }
+    return "Yes";
+}
+
+Console.Write("Input the five-digit number (or input R for a random choice): ");
+string val = Console.ReadLine().ToLower();
+int fiveDigNumb;
+
+if (val == "r") fiveDigNumb = new Random().Next(10000,100000);
+else fiveDigNumb = Convert.ToInt32(val);
+
+Console.WriteLine(fiveDigNumb + " -> " + ifNumberIsPalindrome(fiveDigNumb));
