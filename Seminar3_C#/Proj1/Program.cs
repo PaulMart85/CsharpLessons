16bfe148ed2278aae5f3ee477e019214d7797e72
paul <outimer@mail.ru>
2022-03-03 13:48:43 +0600

    find the quadrant of the point task added

---
+++ Seminar3_C#/Proj1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Написать программу, которая принимает на вход координаты точки (X и Y), 
+// причём X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка.
+Console.Clear();
+
+int NumberOfQuadrant(double xCoord, double yCoord) {
+    if (xCoord > 0 && yCoord > 0) return 1;
+    if (xCoord < 0 && yCoord > 0) return 2;
+    if (xCoord < 0 && yCoord < 0) return 3;
+    if (xCoord > 0 && yCoord < 0) return 4;
+    return 0;
+}
+
+double x, y;
+Console.Write("Input the point (X, Y): X = ");
+x = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Input the point (X, Y): Y = ");
+y = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("The number of quadrant where the point (" + x + ", " + y + ") locates is " + NumberOfQuadrant(x, y));
