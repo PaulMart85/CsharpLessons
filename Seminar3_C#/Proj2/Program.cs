c76a7effb46b118c162e82b4f18d618bd214480a
paul <outimer@mail.ru>
2022-03-03 14:29:32 +0600

    point of quadrant task added

---
+++ Seminar3_C#/Proj2/Program.cs
@@ -0,0 +1,35 @@
+﻿// Написать программу, которая принимает на вход номер четверти и 
+// выводит на экран диапазон координат, доступных в этой четверти
+
+Console.Clear();
+
+void NuberOfQuadrant(int q) {
+    switch (q) {
+        case 1: {
+            Console.WriteLine(" X > 0, Y > 0");
+            break;
+        }   
+        case 2: {
+            Console.WriteLine(" X < 0, Y > 0");
+            break;
+        }
+        case 3: {
+            Console.WriteLine(" X < 0, Y < 0");
+            break;
+        }
+        case 4: {
+            Console.WriteLine(" X > 0, Y < 0");
+            break;
+        }
+        default: {
+            Console.WriteLine(" Quadrant is not defined.");
+            break;
+        }
+    }
+    
+}
+
+Console.Write("Input the number of quadrant: ");
+int qdr = Convert.ToInt32(Console.ReadLine());
+
+NuberOfQuadrant(qdr);
