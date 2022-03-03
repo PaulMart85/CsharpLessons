cae12eef7f1e17cbca054ecaa64f70cfc2527d67
paul <outimer@mail.ru>
2022-03-03 17:58:31 +0600

    3-powered numbers from 1 to N task added

---
+++ Seminar3_C#/Proj6/Program.cs
@@ -0,0 +1,32 @@
+﻿// Task: 3-powered numbers from 1 to N in question
+Console.Clear();
+
+void threePowNumbers(int numb) 
+{
+    int threePowNumb = 1;
+
+    if (numb > 0) 
+    {
+        while (threePowNumb < numb) 
+        {
+            Console.Write(Math.Pow(threePowNumb,3) + ", ");
+            threePowNumb++;
+        }
+        Console.WriteLine(Math.Pow(threePowNumb,3));
+    }
+    else
+    {
+        while (threePowNumb > numb) 
+        {
+            Console.Write(Math.Pow(threePowNumb,3) + ", ");
+            threePowNumb--;
+        }
+        Console.WriteLine(Math.Pow(threePowNumb,3));
+    }
+}
+
+Console.Write("Input the number N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+Console.Write(N + " -> ");
+threePowNumbers(N);
